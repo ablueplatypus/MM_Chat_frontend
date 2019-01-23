3ed37bc92761f9e546eb1dc11c839e9d032f90e4
Matt Doering <ablueplatypus21@gmail.com>
2019-01-23 15:36:58 -0500

    trying to connect websockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,59 @@
 // Welcome MM Chat JS Code.
+// index JS
 document.addEventListener('DOMContentLoaded', (e) => {
 console.log('loaded');
 
+const chatroomContainer = document.querySelector('#chatroom-container')
+
+function chatroomHtml(chatrooms) {
+  return `<div>
+            <ul>
+              <li data-action="room" data-chatroom-id="${chatrooms.id}">${chatrooms.room_name}</li>
+            </ul>
+          </div>
+          `
+}
+
+function chatHtmlText(messages) {
+  return `<div>
+            <ul>
+              <li data-action="">${messages.content}</li>
+            </ul>
+          </div>`
+}
+
+function fetchGetApiChatrooms() {
+  return fetch('http://localhost:3000/api/v1/chatrooms/')
+  .then(res => res.json())
+}
+
+function fetchGetApiMessages() {
+  return fetch('http://localhost:3000/api/v1/messages/')
+  .then(res => res.json())
+}
+
+
+function renderChatrooms() {
+  return fetchGetApiChatrooms().then(chatrooms => {
+    chatrooms.forEach(chatroom => {
+      // console.log(chatroom.room_name)
+      chatroomContainer.innerHTML += chatroomHtml(chatroom)
+    }) //end of forEach
+  })
+}
+
+function onChatroomClick(e) {
+console.log('Data name clicked', e.target.dataset.action, 'target id',e.target.dataset.chatroomId);
+  const data = e.target.dataset
+
+  if (data.action === 'room') {
+    const chatroomId = data.chatroomId
+    console.log(chatroomId);
+
+  }
+
+
+}
 
 
 
@@ -14,8 +66,6 @@ console.log('loaded');
 
 
 
-
-
-
-
+chatroomContainer.addEventListener('click', onChatroomClick)
+renderChatrooms()
 }) // end of DOM DOMContentLoaded event listener
---
+++ src/websocket.js
@@ -0,0 +1,12 @@
+// Websocket JS
+//
+// const API_WS_URL = 'ws://localhost:3000/cable/';
+//
+// let testSocket = new WebSocket(API_WS_URL);
+//
+//     testSocket.onopen = function (event) {
+//       testSocket.send("Can you hear me?");
+//     };
+//     testSocket.onmessage = function (event) {
+//       console.log(event.data);
+//     }
